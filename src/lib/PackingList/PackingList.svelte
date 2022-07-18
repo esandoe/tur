6f345957d9763010aef4201135fef0fac65eab93
Erik Sandø <22426752+esandoe@users.noreply.github.com>
2022-07-18 22:38:37 +0200

    Add some dummy items

--- src/lib/PackingList/PackingList.svelte
+++ src/lib/PackingList/PackingList.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
   import Heading from "../ui/Heading.svelte";
   import Section from "../ui/Section.svelte";
-  import Item from "./Item.svelte";
 
-  let items: string[] = ["Telt", "Sovepose"];
+  let packingList = {
+    categories: [
+      {
+        name: "Søvn",
+        items: ["Telt", "Sovepose + liner", "Liggeunderlag", "Pute"],
+      },
+      {
+        name: "Mat",
+        items: ["Turmiddag", "Pannekakemiks", "Ketchup", "Pølser", "Pølsebrød"],
+      },
+      {
+        name: "Snacks",
+        items: ["Kvikk Lunsj", "Nøtteblanding", "Seigmenn"],
+      },
+      {
+        name: "Klær",
+        items: [
+          "Ullgenser",
+          "Vindtett/vannavstøtende jakke",
+          "Ullsokker",
+          "T-skjorte",
+          "Turshorts",
+        ],
+      },
+    ],
+  };
 </script>
 
 <Section>
   <Heading size="lg">Pakkeliste</Heading>
 
   <div style="display: flex; flex-direction: column; gap: 1em;">
-    {#each items as item}
-      <Item label={item} />
+    {#each packingList.categories as category}
+      <Heading size="sm">{category.name}</Heading>
+      {#each category.items as item}
+        <label>
+          <input type="checkbox" />
+          {item}
+        </label>
+      {/each}
     {/each}
   </div>
 </Section>
